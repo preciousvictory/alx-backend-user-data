e007a05f23c35be21215b4ab4b4cd9e46ab793b9
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-11-07 14:23:19 +0000

    create a class to manage the API authentication.

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" class Auth
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ a class to manage the API authentication """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require_auth"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization_header """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user """
+        return None
