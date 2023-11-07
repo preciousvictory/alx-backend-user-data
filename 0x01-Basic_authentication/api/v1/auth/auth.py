193972c829699fe14c8bf48a15f9bed600b0a988
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-11-07 14:58:51 +0000

    excluded_paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,16 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require_auth"""
-        return False
+        if path is None or excluded_paths is None:
+            return True
+
+        for excluded_path in excluded_paths:
+            if path[-1] != '/':
+                path = '{}/'.format(path)
+
+            if path in excluded_paths:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization_header """
