583409bbc030689337d188270f48d1ea2e5590f5
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-11-07 14:10:47 +0000

    error handler status code 403

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,14 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
+def unauthorized():
     """ GET /api/v1/unauthorized
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """GET /api/v1/forbidden
+    """
+    abort(403)
