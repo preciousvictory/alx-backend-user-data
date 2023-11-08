2874710ddb17a479f9f75450ed790e2dc5d337d3
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-11-08 15:57:15 +0000

    autentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,11 +9,13 @@ import re
 class BasicAuth(Auth):
     """class BasicAuth that inherits from Auth
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, authorization_header: str)\
+            -> str:
         """ returns the Base64 part of the Authorization header
         for a Basic Authentication
         """
-        if authorization_header is None or type(authorization_header) is not str:
+        if authorization_header is None or \
+                type(authorization_header) is not str:
             return None
 
         if authorization_header.split(' ')[0] != 'Basic':
