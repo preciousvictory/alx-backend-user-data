4c61fe456204c918d4018c84ec43d04a31cbc50a
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-11-08 15:16:06 +0000

    flask API

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,6 +16,8 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
+    auth = Auth()
+if auth_type == 'basic_auth':
     auth = BasicAuth()
 
 
