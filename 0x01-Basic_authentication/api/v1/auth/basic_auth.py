a164fba0f512c5503733c6a477a6f8634132c422
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-11-08 16:41:12 +0000

    basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,8 @@
 """
 from flask import request
 from api.v1.auth.auth import Auth
-import re
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -20,5 +21,21 @@ class BasicAuth(Auth):
 
         if authorization_header.split(' ')[0] != 'Basic':
             return None
-
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """ returns the decoded value of a Base64 string
+        base64_authorization_header
+        """
+        if base64_authorization_header is None or \
+                type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+        except binascii.Error:
+            return None
+
+        return decoded.decode('utf-8')
