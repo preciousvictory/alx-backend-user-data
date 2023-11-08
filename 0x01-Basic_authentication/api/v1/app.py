2a9682a22efa4ceab87a7273c87e4e159b690b2d
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-11-08 15:14:09 +0000

    flask API

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -16,7 +16,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
-    auth = Auth()
+    auth = BasicAuth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" class BasicAuth
+"""
+from flask import request
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """class BasicAuth that inherits from Auth"""
+    pass
