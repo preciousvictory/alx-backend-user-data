c352d8c65e252eba45971c7bf42c21216eba79ea
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-11-08 15:46:54 +0000

    autentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,8 +3,20 @@
 """
 from flask import request
 from api.v1.auth.auth import Auth
+import re
 
 
 class BasicAuth(Auth):
-    """class BasicAuth that inherits from Auth"""
-    pass
+    """class BasicAuth that inherits from Auth
+    """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ returns the Base64 part of the Authorization header
+        for a Basic Authentication
+        """
+        if authorization_header is None or type(authorization_header) is not str:
+            return None
+
+        if authorization_header.split(' ')[0] != 'Basic':
+            return None
+
+        return authorization_header.split(' ')[1]
