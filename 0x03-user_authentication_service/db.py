c3c32b0b447b94b55a132441d5fb08685814873a
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-11-17 17:32:21 +0000

    user authentication service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from user import Base
+from user import Base, User
 
 
 class DB:
@@ -30,10 +30,9 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """implement the add_user method.
         """
-        from user import User
         try:
             new_user = User(email=email, hashed_password=hashed_password)
             self._session.add(new_user)
